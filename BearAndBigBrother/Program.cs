c2270fbd1aadcb1b4cd1585f818afe755aad0f7f
MohamedFeky <mohamedelfeky777@gmail.com>
2025-07-21 22:30:45 +0300

    Solved problem Bear and Big Brother and problem Team.

---
+++ BearAndBigBrother/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace BearAndBigBrother
+{
+    internal class Program
+    {
+        public static int CountWeight(int a, int b)
+        {
+            int count = 0;
+            if (1 <= a && a <= b && b <= 10)
+            {
+                while (a <= b)
+                {
+                    a = a * 3;
+                    b = b * 2;
+                    count++;
+                }
+              
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+
+            Console.WriteLine(CountWeight(a,b));
+        }
+    }
+}
---
+++ Team/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Team
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+           
+            while(n > 0)
+            {
+
+                    string[] input = Console.ReadLine().Split(' ');
+                    int petya = int.Parse(input[0]);
+                    int vasya = int.Parse(input[1]);
+                    int tonya = int.Parse(input[2]);
+
+                if(petya + vasya +tonya == 2 || petya + vasya + tonya == 3)
+                {
+                    count++;
+                }    
+                n--;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
